4a271bf40de082b0f625273414c99c9fa1d2bde9
Aiko <aikocute@icloud.com>
2022-04-16 20:12:40 +0700

    Update aiko.sh

--- aiko.sh
+++ aiko.sh
@@ -322,7 +322,7 @@ install_XrayR() {
     echo -e "${green}Node ID của bạn đặt là: ${aiko_node_id}${plain}"
     echo -e "-------------------------"
 
-    wget https://raw.githubusercontent.com/AikoCute/Aiko-Config/aiko/Config-Trojan%2BVmess.yml -O /etc/XrayR/config.yml
+    wget https://raw.githubusercontent.com/AikoCute/Aiko-Config/aiko/Config-V2ray.yml -O /etc/XrayR/config.yml
     sed -i "s/NodeID:.*/NodeID: ${aiko_node_id}/g" /etc/XrayR/config.yml
     
     fi
